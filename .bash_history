05dfb9f0cbe7fc4463972a33ae7379787b024a46
Gerbido <gerberti@libero.it>
2024-08-10 23:52:39 +0200

    Primo commit

---
+++ .bash_history
@@ -0,0 +1,24 @@
+sudo apt-get update
+sudo apt-get install default-jdk
+java-versione
+java --version
+shell
+sudo su
+cd /usr /src
+cd/usr/src
+cd /usr/src
+sudo wget https:www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz
+[200~wget https:www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz~
+get https:www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz
+wget https:www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz
+/bin/python3 "/home/gerberti/print ("ciao mondo").py"
+sudo apt update
+node --version
+sudo apt update
+sudo apt install nodejs npm
+node --version
+npm --version
+mkdir png-manager
+cd png-manager
+npx create-react-app .
+npm start
---
+++ .config/Code/User/History/5d9530f0/UF0K.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Gestore PNG</h1>
+      <p>Benvenuto nel tuo gestore di Personaggi Non Giocanti!</p>
+    </div></code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
